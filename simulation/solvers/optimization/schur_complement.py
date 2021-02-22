538398c717817c6d93e07059f8874a1806cbbe61
prbartman <prbartman@gmail.com>
2021-02-22 15:31:06 +0100

    finished schur_complement.py refactor

--- simulation/solvers/optimization/schur_complement.py
+++ simulation/solvers/optimization/schur_complement.py
@@ -13,10 +13,8 @@ class SchurComplement(Optimization):
     def __init__(self, grid, inner_forces, outer_forces, mu_coef, lambda_coef, contact_law, friction_bound):
         super().__init__(grid, inner_forces, outer_forces, mu_coef, lambda_coef, contact_law, friction_bound)
 
-        contact_points_num = grid.contact_num
-        independent_points_num = grid.independent_num
-        contact_ids = slice(0, contact_points_num)
-        free_ids = slice(contact_points_num, independent_points_num)
+        contact_ids = slice(0, grid.contact_num)
+        free_ids = slice(grid.contact_num, grid.independent_num)
 
         free_x_free = SchurComplement.get_submatrix(self.B, indices=(free_ids, free_ids))
         free_x_contact = SchurComplement.get_submatrix(self.B, indices=(free_ids, contact_ids))
@@ -52,25 +50,31 @@ class SchurComplement(Optimization):
         return self.__point_forces
 
     def solve(self, initial_guess: np.ndarray) -> np.ndarray:
+        truncated_initial_guess = self.truncate_free_points(initial_guess)
+        solution_contact = super().solve(truncated_initial_guess)
+        solution_free = self.complement_free_points(solution_contact)
+        solution = self.merge(solution_contact, solution_free)
+        return solution
 
-        c_num = self.grid.BorderEdgesC
+    def truncate_free_points(self, initial_guess):
+        _result = initial_guess.reshape(2, -1)
+        _result = _result[:, 0: self.grid.contact_num]
+        _result = _result.reshape(1, -1)
+        result = _result
+        return result
 
-        x = initial_guess.reshape(2,-1)
-        y = x[:, 0: c_num]
-        z = y.reshape(1,-1)
-        ut_vector = z
+    def complement_free_points(self, truncated_solution):
+        _result = truncated_solution.reshape(-1, 1)
+        _result = np.dot(self.free_x_contact, _result)
+        _result = self.forces_free - _result
+        result = np.dot(self.free_x_free_inverted, _result)
+        return result
 
-        ut_vector = super().solve(ut_vector)
-
-        ut_v = ut_vector.reshape(-1,1)
-        first = np.dot(self.free_x_contact, ut_v)
-        second = self.forces_free - first
-        ui_vector = np.dot(self.free_x_free_inverted, second)
-
-        ut = ut_vector.reshape(2, -1)
-        ui = ui_vector.reshape(2, -1)
-        result = np.concatenate((ut, ui), axis=1)
-        result = result.reshape(1, -1)
-        result = np.squeeze(np.asarray(result))
-
+    @staticmethod
+    def merge(solution_contact, solution_free):
+        u_contact = solution_contact.reshape(2, -1)
+        u_free = solution_free.reshape(2, -1)
+        _result = np.concatenate((u_contact, u_free), axis=1)
+        _result = _result.reshape(1, -1)
+        result = np.squeeze(np.asarray(_result))
         return result
