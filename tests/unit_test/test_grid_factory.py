8b4c5a204b2195844667c73536ceeb3125021508
prbartman <prbartman@gmail.com>
2021-04-13 23:39:04 +0200

    refactor tests for grid factory

--- tests/unit_test/test_grid_factory.py
+++ tests/unit_test/test_grid_factory.py
@@ -3,6 +3,7 @@ Created at 12.04.2021
 """
 
 import numpy as np
+import pytest
 from simulation.grid import Grid
 from simulation.grid_factory import GridFactory
 from simulation.point import Point
@@ -12,60 +13,11 @@ from simulation.edge import Edge
 class TestGridFactory:
 
     @staticmethod
-    def test_add_single_point():
-        # Arrange
-        grid = Grid()
-        grid.Edges = np.zeros((1, 3))
-        x = 0.0
-        y = 0.0
-        t = Grid.CROSS
-
-        # Act
-        GridFactory.add_point(grid, x, y, t)
-
-        # Assert
-        assert 1 == len(grid.Points)
-        assert np.all(grid.Edges[:, Edge.TYPE] == 0)
-        assert np.all(grid.Edges[:, Edge.START] == 1)
-        assert np.all(grid.Edges[:, Edge.STOP] == 1)
-        point = grid.Points[0]
-        assert x == point[Point.X]
-        assert y == point[Point.Y]
-        assert t == point[Point.TYPE]
-
-    @staticmethod
-    def test_add_multiple_points():
+    @pytest.mark.parametrize("length, repeated", ((1, 0), (6, 0), (9, 3)))
+    def test_add_multiple_points_with_repeats(length, repeated):
         # Arrange
         grid = Grid()
         grid.Edges = np.zeros((5, 3))
-        length = 6
-        x = [0.0, 1, 3, 0, -1, -3.14]
-        y = [0.0, 1, -2.3, 5,16, -10, 1]
-        t = [Grid.CROSS, Grid.TOP, Grid.BOTTOM, Grid.RIGHT_BOTTOM_CORNER, Grid.LEFT_BOTTOM_CORNER, Grid.NORMAL_MIDDLE]
-
-        # Act
-        for i in range(length):
-            GridFactory.add_point(grid, x[i], y[i], t[i])
-
-        # Assert
-        assert length == len(grid.Points)
-        print(grid.Edges)
-        assert np.all(grid.Edges[:, Edge.TYPE] == 0)
-        assert np.all(grid.Edges[:, Edge.START] == length)
-        assert np.all(grid.Edges[:, Edge.STOP] == length)
-        for i in range(length):
-            point = grid.Points[i]
-            assert x[length - i - 1] == point[Point.X]
-            assert y[length - i - 1] == point[Point.Y]
-            assert t[length - i - 1] == point[Point.TYPE]
-
-    @staticmethod
-    def test_add_multiple_points_with_repeats():
-        # Arrange
-        grid = Grid()
-        grid.Edges = np.zeros((5, 3))
-        length = 9
-        repeated = 3
         x = [0.0, 1,    3, 0, -1, -3.14, 0, -1, -3.14]
         y = [0.0, 1, -2.3, 5, 16,   -10, 5, 16,   -10]
         t = [Grid.CROSS, Grid.TOP, Grid.BOTTOM,
@@ -77,12 +29,13 @@ class TestGridFactory:
             GridFactory.add_point(grid, x[i], y[i], t[i])
 
         # Assert
-        assert length - repeated == len(grid.Points)
+        points_num = length - repeated
+        assert points_num == len(grid.Points)
         assert np.all(grid.Edges[:, Edge.TYPE] == 0)
-        assert np.all(grid.Edges[:, Edge.START] == length - repeated)
-        assert np.all(grid.Edges[:, Edge.STOP] == length - repeated)
-        for i in range(length - repeated):
+        assert np.all(grid.Edges[:, Edge.START] == points_num)
+        assert np.all(grid.Edges[:, Edge.STOP] == points_num)
+        for i in range(points_num):
             point = grid.Points[i]
-            assert x[length - repeated - i - 1] == point[Point.X]
-            assert y[length - repeated - i - 1] == point[Point.Y]
-            assert t[length - repeated - i - 1] == point[Point.TYPE]
+            assert x[points_num - i - 1] == point[Point.X]
+            assert y[points_num - i - 1] == point[Point.Y]
+            assert t[points_num - i - 1] == point[Point.TYPE]
