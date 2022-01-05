99517423f5dabf1d1155a0a5bf0cef6ebfef1fb0
Piotr Bartman <prbartman@gmail.com>
2022-01-05 19:38:18 +0100

    example_dynamic.py cleanup

--- examples/example_dynamic.py
+++ examples/example_dynamic.py
@@ -15,14 +15,14 @@ from utils.drawer import Drawer
 class DynamicSetup(Dynamic):
     grid_height: ... = 1
     cells_number: ... = (2, 5)
-    inner_forces: ... = np.array([0, 0.2])
+    inner_forces: ... = np.array([-0.2, -0.2])
     outer_forces: ... = np.array([0, 0])
     mu_coef: ... = 4
     lambda_coef: ... = 4
     th_coef: ... = 4
     ze_coef: ... = 4
     time_step: ... = 0.1
-    contact_law: ... = make_slope_contact_law(slope=0)
+    contact_law: ... = make_slope_contact_law(slope=1e1)
 
     @staticmethod
     def friction_bound(u_nu):
@@ -34,9 +34,5 @@ if __name__ == '__main__':
     runner = DynamicProblemSolver(setup, solving_method='schur')
 
     states = runner.solve(n_steps=32, output_step=(0, 32), verbose=True)
-    displacement_vector = states[-1].displacement.T.reshape(1, -1)[0]
-    import numpy
-    numpy.set_printoptions(precision=8, suppress=True)
-    print(repr(displacement_vector))
     for state in states:
         Drawer(state).draw()
