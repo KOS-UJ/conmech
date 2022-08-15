4e5b50cc64a02f48921aa20d5347debed4af27c7
Piotr <prbartman@gmail.com>
2022-08-15 16:20:49 +0200

    remove meshzoo ball

--- conmech/mesh/mesh_builders.py
+++ conmech/mesh/mesh_builders.py
@@ -58,7 +58,7 @@ def build_initial_mesh(
             if "cube" in mesh_prop.mesh_type:
                 return mesh_builders_3d.get_test_cube(mesh_prop)
             if "ball" in mesh_prop.mesh_type:
-                return mesh_builders_3d.get_meshzoo_ball(mesh_prop)
+                return mesh_builders_3d.get_test_ball(mesh_prop)
         else:
             return mesh_builders_2d.get_meshzoo_rectangle(mesh_prop)
 
