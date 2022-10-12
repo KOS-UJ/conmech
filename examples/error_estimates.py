ac0c1cb74d5d6f22086f167f1ed66d165d661e3c
Piotr <prbartman@gmail.com>
2022-10-12 22:15:04 +0200

    + derivatives

--- examples/error_estimates.py
+++ examples/error_estimates.py
@@ -1,3 +1,4 @@
+import numba
 import numpy as np
 from scipy import interpolate
 import pickle
@@ -37,12 +38,38 @@ def compare(ref: TemperatureState, sol: TemperatureState):
         t2 = ref.temperature[element[2]]
         t = t0 + t1 + t2
 
+        u1dx, u1dy = calculate_dx_dy(x0, u1_0, x1, u1_1, x2, u1_2)
+        u2dx, u2dy = calculate_dx_dy(x0, u2_0, x1, u2_1, x2, u2_2)
+        tdx, tdy = calculate_dx_dy(x0, t0, x1, t1, x2, t2)
+        u1hdx, u1hdy = calculate_dx_dy(x0, u1hi(*x0), x1, u1hi(*x1), x2, u1hi(*x2))
+        u2hdx, u2hdy = calculate_dx_dy(x0, u2hi(*x0), x1, u2hi(*x1), x2, u2hi(*x2))
+        thdx, thdy = calculate_dx_dy(x0, thi(*x0), x1, thi(*x1), x2, thi(*x2))
+
+
         u1h = u1hi(*x0) + u1hi(*x1) + u1hi(*x2)
         u2h = u2hi(*x0) + u2hi(*x1) + u2hi(*x2)
-        ut += np.linalg.norm(u1 - u1h) + np.linalg.norm(u2 - u2h)
+        ut += (u1 - u1h)**2 + (u2 - u2h)**2 \
+              + (u1dx - u1hdx)**2 + (u2dx - u2hdx)**2 \
+              + (u1dy - u1hdy)**2 + (u2dy - u2hdy)**2
+
         th = thi(*x0) + thi(*x1) + thi(*x2)
-        tt += np.linalg.norm(t - th)
+        tt += (t - th)**2 + (tdx - thdx)**2 + (tdy - thdy)**2
     return ut, tt
+
+@numba.njit()
+def calculate_dx_dy(x0, u0, x1, u1, x2, u2):
+    a1 = x1[0] - x0[0]
+    b1 = x1[1] - x0[1]
+    c1 = u1 - u0
+    a2 = x2[0] - x0[0]
+    b2 = x2[1] - x0[1]
+    c2 = u2 - u0
+    a = b1 * c2 - b2 * c1
+    b = a2 * c1 - a1 * c2
+    c = a1 * b2 - b1 * a2
+    dx = a / c
+    dy = b / c
+    return dx, dy
 
 
 if __name__ == "__main__":
