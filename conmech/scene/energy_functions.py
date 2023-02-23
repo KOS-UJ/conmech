Fixed bug in scene_input and added nan_to_num in friction calculation

conmech/scene/energy_functions.py
@@ -46,7 +46,7 @@ def _obstacle_resistance_tangential_vector(
         epsilon + nph.euclidean_norm(tangential_velocity, keepdims=True)
     )
     result = (initial_penetration > 0) * friction * friction_law
-    return result  # TODO: Check this: jnp.nan_to_num(result)  # otherwise Nans in division
+    return jnp.nan_to_num(result)  # TODO: Check this, otherwise Nans in division
 
 
 class EnergyObstacleArguments(NamedTuple):

deep_conmech/scene/scene_input.py
@@ -63,9 +63,6 @@ class SceneInput(SceneRandomized):
             simulation_config=simulation_config,
             create_in_subprocess=create_in_subprocess,
         )
-        self.cached_nodes_column = None
-        self.cached_reduced_nodes_column = None
-        self.cached_nodes_multilayer_column = None
 
     @mesh_normalization_decorator
     def get_edges_data(self, directional_edges, reduced=False):
@@ -80,22 +77,17 @@ class SceneInput(SceneRandomized):
             )
 
         if reduced:
-            if self.cached_reduced_nodes_column is None:
-                self.cached_reduced_nodes_column = get_column(scene.input_initial_nodes)
-
             return jnp.hstack(
                 (
-                    self.cached_reduced_nodes_column,  # get_column(scene.input_initial_nodes),
+                    get_column(scene.input_initial_nodes),  # cached
                     get_column(scene.input_displacement_old),
                     get_column(scene.input_velocity_old),
                     get_column(scene.input_forces),
                 )
             )
-        if self.cached_nodes_column is None:
-            self.cached_nodes_column = get_column(scene.input_initial_nodes)
         return jnp.hstack(
             (
-                scene.cached_nodes_column,  # get_column(scene.input_initial_nodes),
+                get_column(scene.input_initial_nodes),
                 get_column(scene.input_forces),
             )
         )
@@ -114,13 +106,9 @@ class SceneInput(SceneRandomized):
                 directional_edges=directional_edges,
             )
 
-        if self.cached_nodes_multilayer_column is None:
-            self.cached_nodes_multilayer_column = get_column(
-                self.reduced.input_forces, self.input_forces
-            )
         return np.hstack(
             (
-                self.cached_nodes_multilayer_column,  # get_column(self.reduced.input_initial_nodes, self.input_initial_nodes),
+                get_column(self.reduced.input_initial_nodes, self.input_initial_nodes),  # cached
                 # get_column(
                 #     displacement_old_sparse,
                 #     displacement_old_dense,

examples/draft_examples_3d.py
@@ -56,6 +56,7 @@ def main():
         use_pca=True,
         mode=mode,
     )
+    final_time = 2.5
     scale_forces = 5.0
 
     # all_print_scenaros = scenarios.all_print(config.td, config.sc)
@@ -65,12 +66,24 @@ def main():
         # bunny_obstacles(
         #     mesh_density=64,
         #     scale=1,
-        #     final_time=10,
+        #     final_time=final_time,
         #     simulation_config=simulation_config,
         #     scale_forces=scale_forces,
         # ),
-        bunny_fall_3d(mesh_density=64, scale=1, final_time=10, simulation_config=simulation_config),
-        # bunny_fall_3d(mesh_density=32, scale=1, final_time=10, simulation_config=simulation_config),
+        # bunny_fall_3d(
+        #     mesh_density=64,
+        #     scale=1,
+        #     final_time=final_time,
+        #     simulation_config=simulation_config,
+        #     scale_forces=scale_forces,
+        # ),
+        bunny_fall_3d(
+            mesh_density=32,
+            scale=1,
+            final_time=final_time,
+            simulation_config=simulation_config,
+            scale_forces=scale_forces,
+        ),
         # bunny_fall_3d(
         #     mesh_density=16,
         #     scale=1,
@@ -164,29 +177,29 @@ def main():
         #         np.array([[[0.0, 0.0, 1.0]], [[0.0, 0.0, 0.3]]]), default_obstacle_prop
         #     ),
         # )
-        Scenario(
-            name="armadillo_fall",
-            mesh_prop=MeshProperties(
-                dimension=3,
-                mesh_type=M_ARMADILLO_3D,
-                scale=[1],
-                mesh_density=[16],
-            ),
-            body_prop=TimeDependentBodyProperties(
-                mu=8 * 2,
-                lambda_=8 * 2,
-                theta=4 * 2,
-                zeta=4 * 2,
-                mass_density=1.0,
-            ),
-            schedule=Schedule(final_time=10, time_step=0.01),
-            forces_function=scale_forces * np.array([0.0, 0.0, -1.0]),
-            obstacle=Obstacle(  # 0.3
-                np.array([[[0.7, 0.0, 1.0]], [[1.0, 1.0, 0.0]]]),
-                ObstacleProperties(hardness=100.0, friction=0.1),  # 5.0),
-            ),
-            simulation_config=simulation_config,
-        ),
+        # Scenario(
+        #     name="armadillo_fall",
+        #     mesh_prop=MeshProperties(
+        #         dimension=3,
+        #         mesh_type=M_ARMADILLO_3D,
+        #         scale=[1],
+        #         mesh_density=[16],
+        #     ),
+        #     body_prop=TimeDependentBodyProperties(
+        #         mu=8 * 2,
+        #         lambda_=8 * 2,
+        #         theta=4 * 2,
+        #         zeta=4 * 2,
+        #         mass_density=1.0,
+        #     ),
+        #     schedule=Schedule(final_time=10, time_step=0.01),
+        #     forces_function=scale_forces * np.array([0.0, 0.0, -1.0]),
+        #     obstacle=Obstacle(  # 0.3
+        #         np.array([[[0.7, 0.0, 1.0]], [[1.0, 1.0, 0.0]]]),
+        #         ObstacleProperties(hardness=100.0, friction=0.1),  # 5.0),
+        #     ),
+        #     simulation_config=simulation_config,
+        # ),
     ]
 
     simulation_runner.run_examples(