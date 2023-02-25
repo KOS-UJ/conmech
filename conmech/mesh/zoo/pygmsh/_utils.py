5bf43ba79ea71dc99f45e5f5d1d1e95775fe2cef
Piotr <prbartman@gmail.com>
2023-02-25 07:29:59 +0100

    move meshes to "zoo"

---
+++ conmech/mesh/zoo/pygmsh/_utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from conmech.properties.mesh_properties import MeshProperties
+from deep_conmech.data.interpolation_helpers import interpolate_nodes
+
+
+def get_random_corner_data():
+    return np.random.rand(4).reshape(-1, 1)
+    # random_data = np.zeros(4) #random_data[1] = 1.
+
+
+def get_random_corner_mesh_size(mesh_prop: MeshProperties):
+    mesh_density = mesh_prop.mesh_density_x
+    scale = mesh_density * 0.8
+    corner_data = (mesh_prop.corner_mesh_data * 2.0 * scale) - scale
+    return 1.0 / (mesh_density + corner_data)
+
+
+# CORNERS left, bottom, right, top
+def set_mesh_size(geom, mesh_prop: MeshProperties):
+    if mesh_prop.corner_mesh_data is not None:
+        if mesh_prop.dimension != 2:
+            raise NotImplementedError
+        corner_vectors = get_random_corner_mesh_size(mesh_prop=mesh_prop)
+        callback = lambda dim, tag, x, y, z, *_: interpolate_nodes(
+            scaled_nodes=np.array([[x / mesh_prop.scale_x, y / mesh_prop.scale_y]]),
+            corner_vectors=corner_vectors,
+        ).item()
+    else:
+        callback = lambda dim, tag, x, y, z, *_: 1.0 / mesh_prop.mesh_density_x
+
+    geom.set_mesh_size_callback(callback)
+
+
+def normalize_nodes(nodes):
+    nodes = nodes - np.min(nodes, axis=0)
+    nodes = nodes / np.max(nodes, axis=0)
+    return nodes
+
+
+def get_nodes_and_elements(geom, dim):
+    geom_mesh = geom.generate_mesh()
+    nodes = geom_mesh.points.copy()
+    elements = geom_mesh.cells[-2].data.astype("long").copy()
+    return nodes[:, :dim], elements
---
+++ conmech/mesh/zoo/pygmsh/dim_2/circle.py
@@ -0,0 +1,37 @@
+# CONMECH @ Jagiellonian University in Kraków
+#
+# Copyright (C) 2023  Piotr Bartman <piotr.bartman@uj.edu.pl>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+import pygmsh
+
+from conmech.mesh.zoo import MeshZOO
+from conmech.mesh.zoo.pygmsh import _utils
+from conmech.mesh.zoo.raw_mesh import RawMesh
+from conmech.properties.mesh_properties import MeshProperties
+
+
+@MeshZOO.register("circle", "pygmsh_circle", "pygmsh_circle_2d")
+class Circle(RawMesh):
+    def __init__(self, mesh_prop: MeshProperties):
+        with pygmsh.geo.Geometry() as geom:
+            geom.add_circle(
+                [mesh_prop.scale_x / 2.0, mesh_prop.scale_y / 2.0],
+                mesh_prop.scale_x / 2.0,
+            )
+        _utils.set_mesh_size(geom, mesh_prop)
+        nodes, elements = _utils.get_nodes_and_elements(geom, 2)
+        super().__init__(nodes, elements)
