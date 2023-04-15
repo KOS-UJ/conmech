Fixed rotation calculation, added dataset statistics to JAX model

conmech/dynamics/dynamics.py
@@ -92,7 +92,7 @@ def _get_deform_grad(value, dx_big_jax):
     return _get_jac(value, dx_big_jax) + identity
 
 
-class _GetRotationState(NamedTuple):
+class RotationState(NamedTuple):
     rotation: jnp.ndarray
     norm: float
     iteration: int
@@ -101,8 +101,8 @@ class _GetRotationState(NamedTuple):
 
 @jax.jit
 def _get_rotation_jax(displacement, dx_big):
-    max_iter = 30
-    max_norm = 1e-4
+    max_iter = 100
+    max_norm = 5e-4
     deform_grad = _get_deform_grad(displacement, dx_big)
 
     def body(state):
@@ -110,16 +110,16 @@ def _get_rotation_jax(displacement, dx_big):
         rotation_new = 0.5 * (state.rotation + rotation_inv_T)
         norm = jnp.linalg.norm(state.rotation - rotation_new)
         iteration = state.iteration + 1
-        return _GetRotationState(
+        return RotationState(
             rotation=rotation_new, norm=norm, iteration=iteration, success=iteration < max_iter
         )
 
-    state = _GetRotationState(rotation=deform_grad, norm=0, iteration=0, success=True)
+    state = RotationState(rotation=deform_grad, norm=jnp.inf, iteration=0, success=True)
     state = lax.while_loop(
         lambda state: (state.norm > max_norm) & (state.iteration < max_iter), body, state
     )
     final_rotation = jnp.linalg.inv(np.mean(state.rotation, axis=0))
-    return final_rotation, state.success
+    return final_rotation, state
 
 
 class Dynamics(BodyPosition):
@@ -157,10 +157,11 @@ class Dynamics(BodyPosition):
         self.moved_base = self.get_rotation(self.displacement_old)
 
     def get_rotation(self, displacement):
-        result = _get_rotation_jax(displacement, self.matrices.dx_big_jax)
-        if not result[1]:
+        final_rotation, state = _get_rotation_jax(displacement, self.matrices.dx_big_jax)
+        if not state.success:
             raise Exception("Error calculating rotation")
-        return complete_base(base_seed=np.array(result[0], dtype=np.float64))
+        print(state.iteration, state.norm)
+        return complete_base(base_seed=np.array(final_rotation, dtype=np.float64))
 
     # def iterate_self(self, acceleration, temperature=None):
     #     super().iterate_self(acceleration, temperature)

deep_conmech/graph/net_jax.py
@@ -2,7 +2,7 @@ import copy
 import random
 from ctypes import ArgumentError
 from functools import partial
-from typing import List, NamedTuple, Optional, Tuple
+from typing import Callable, List, NamedTuple, Optional, Tuple
 
 import jax
 import jax.dlpack
@@ -16,9 +16,22 @@ from conmech.helpers import cmh, lnh, pkh
 from conmech.mesh.mesh import Mesh
 from conmech.scene.energy_functions import EnergyFunctions
 from conmech.solvers.calculator import Calculator
+from deep_conmech.data.dataset_statistics import DatasetStatistics, FeaturesStatistics
 from deep_conmech.helpers import thh
 from deep_conmech.scene.scene_input import SceneInput
 from deep_conmech.training_config import CLOSEST_COUNT, TrainingData
+
+
+class DataNorm(nn.Module):
+    std_init: Callable
+
+    @nn.compact
+    def __call__(self, x):
+        std = self.variable("batch_stats", "std", self.std_init, x.shape[1])
+        # x_std.value += 0.1 # Could be modified here and will be saved
+        output = x / std.value
+        output = jnp.nan_to_num(output)
+        return output
 
 
 class ForwardNet(nn.Module):
@@ -193,6 +206,8 @@ class GraphNetArguments(NamedTuple):
 
 
 class CustomGraphNetJax(nn.Module):
+    statistics: Optional[DatasetStatistics] = None
+
     @nn.compact
     def __call__(self, args: GraphNetArguments, train: bool):
         latent_dimension = 128  # 128 64
@@ -223,8 +238,8 @@ class CustomGraphNetJax(nn.Module):
             latent_dimension,
             node_latents_sparse,
             node_latents_dense,
-            edge_latents_to_dense,
-            edge_index_to_dense,
+            edge_latents_multilayer,
+            edge_index_multilayer,
         ):
             updated_node_latents_dense = LinkProcessorLayer(
                 latent_dimension=latent_dimension,
@@ -233,40 +248,73 @@ class CustomGraphNetJax(nn.Module):
             )(
                 node_latents_sparse=node_latents_sparse,
                 node_latents_dense=node_latents_dense,
-                edge_latents_to_dense=edge_latents_to_dense,
-                edge_index_to_dense=edge_index_to_dense,
+                edge_latents_to_dense=edge_latents_multilayer,
+                edge_index_to_dense=edge_index_multilayer,
             )
             return updated_node_latents_dense
+
+        node_data_sparse = DataNorm(
+            std_init=lambda _: jnp.array(self.statistics.data[0].data_max_abs)
+        )(args.sparse_x)
+        edge_data_sparse = DataNorm(
+            std_init=lambda _: jnp.array(self.statistics.data[1].data_max_abs)
+        )(args.sparse_edge_attr)
+        edge_data_multilayer = DataNorm(
+            std_init=lambda _: jnp.array(self.statistics.data[2].data_max_abs)
+        )(args.multilayer_edge_attr)
+        node_data_dense = DataNorm(
+            std_init=lambda _: jnp.array(self.statistics.data[3].data_max_abs)
+        )(args.dense_x)
+        edge_data_dense = DataNorm(
+            std_init=lambda _: jnp.array(self.statistics.data[4].data_max_abs)
+        )(args.dense_edge_attr)
+
+        # std = 1.0
+        # node_data_sparse = DataNorm(
+        #     std_init=lambda _: None  # jnp.array([std for _ in range(args.sparse_x.shape[1])])
+        # )(args.sparse_x)
+        # edge_data_sparse = DataNorm(
+        #     std_init=lambda _: jnp.array([std for _ in range(args.sparse_edge_attr.shape[1])])
+        # )(args.sparse_edge_attr)
+        # edge_data_multilayer = DataNorm(
+        #     std_init=lambda _: jnp.array([std for _ in range(args.multilayer_edge_attr.shape[1])])
+        # )(args.multilayer_edge_attr)
+        # node_data_dense = DataNorm(
+        #     std_init=lambda _: jnp.array([std for _ in range(args.dense_x.shape[1])])
+        # )(args.dense_x)
+        # edge_data_dense = DataNorm(
+        #     std_init=lambda _: jnp.array([std for _ in range(args.dense_edge_attr.shape[1])])
+        # )(args.dense_edge_attr)
 
         node_latents_sparse = ForwardNet(
             latent_dimension=latent_dimension,
             internal_layer_count=internal_layer_count,
             input_batch_norm=input_batch_norm,
-        )(args.sparse_x, train=train)
+        )(node_data_sparse, train=train)
 
         edge_latents_sparse = ForwardNet(
             latent_dimension=latent_dimension,
             internal_layer_count=internal_layer_count,
             input_batch_norm=input_batch_norm,
-        )(args.sparse_edge_attr, train=train)
+        )(edge_data_sparse, train=train)
 
-        edge_latents_to_dense = ForwardNet(
+        edge_latents_multilayer = ForwardNet(
             latent_dimension=latent_dimension,
             internal_layer_count=internal_layer_count,
             input_batch_norm=input_batch_norm,
-        )(args.multilayer_edge_attr, train=train)
+        )(edge_data_multilayer, train=train)
 
         node_latents_dense = ForwardNet(
             latent_dimension=latent_dimension,
             internal_layer_count=internal_layer_count,
             input_batch_norm=input_batch_norm,
-        )(args.dense_x, train=train)
+        )(node_data_dense, train=train)
 
         edge_latents_dense = ForwardNet(
             latent_dimension=latent_dimension,
             internal_layer_count=internal_layer_count,
             input_batch_norm=input_batch_norm,
-        )(args.dense_edge_attr, train=train)
+        )(edge_data_dense, train=train)
 
         updated_node_latents_sparse = node_latents_sparse + propagate_messages(
             latent_dimension=latent_dimension,
@@ -290,8 +338,8 @@ class CustomGraphNetJax(nn.Module):
             latent_dimension=latent_dimension,
             node_latents_sparse=updated_node_latents_sparse,
             node_latents_dense=node_latents_dense,
-            edge_latents_to_dense=edge_latents_to_dense,
-            edge_index_to_dense=args.multilayer_edge_index,
+            edge_latents_multilayer=edge_latents_multilayer,
+            edge_index_multilayer=args.multilayer_edge_index,
         )
 
         updated_node_latents_dense = updated_node_latents_dense + propagate_messages(

deep_conmech/run_model.py
@@ -6,6 +6,7 @@ import argparse
 import os
 from argparse import ArgumentParser, Namespace
 from ctypes import ArgumentError
+from pathlib import Path
 
 import jax
 import netron
@@ -126,7 +127,7 @@ def train_single(config, rank=0, world_size=1, train_dataset=None, all_validatio
             all_validation_datasets=all_validation_datasets,
             print_scenarios=all_print_datasets,
             config=config,
-            statistics=None,
+            statistics=statistics,
         )
     else:
         net = CustomGraphNet(statistics=statistics, td=config.td).to(rank)
@@ -300,13 +301,13 @@ def get_newest_checkpoint_path_jax(config: TrainingConfig):
     def get_index_jax(path):
         return int(path.split("/")[-2].split(" ")[0])
 
-    saved_model_paths = cmh.find_files_by_name(config.output_catalog, "checkpoint_0")
-    if not saved_model_paths:
+    all_checkpoint_paths = cmh.find_files_by_name(config.output_catalog, "checkpoint")
+    if not all_checkpoint_paths:
         raise ArgumentError("No saved models")
-    newest_index = np.argmax(np.array([get_index_jax(path) for path in saved_model_paths]))
-    path = saved_model_paths[newest_index]
+    newest_index = np.argmax(np.array([get_index_jax(path) for path in all_checkpoint_paths]))
 
-    print(f"Taking saved model {path.split('/')[-2]}")
+    path = str(Path(all_checkpoint_paths[newest_index]).parent.absolute())
+    print(f"Taking saved model {path.split('/')[-1]}")
     return path
 
 
@@ -345,7 +346,7 @@ def main(args: Namespace):
         with_self_collisions=True,
         mesh_layer_proportion=4,  # 2 4
         use_pca=False,
-        mode="normal",        # mode="normal" ##############
+        mode="normal",
     )
 
     # dch.set_torch_sharing_strategy()