40509a7a35b6db6e8a8ec147de502480ea2c47d5
Piotr <prbartman@gmail.com>
2026-06-29 23:17:26 +0200

    BOT 2026: init

--- examples/example_Tarzia_problem.py
+++ examples/example_Tarzia_problem.py
@@ -9,8 +9,10 @@ from conmech.plotting.drawer import Drawer
 from conmech.scenarios.problems import PoissonProblem
 from conmech.dynamics.contact.contact_law import ContactLaw, PotentialOfContactLaw
 from conmech.simulations.problem_solver import PoissonSolver
-from conmech.properties.mesh_description import CrossMeshDescription
+from conmech.properties.mesh_description import RectangleMeshDescription
 
+
+B_COEF = 5
 
 def make_slope_contact_law(slope: float) -> Type[ContactLaw]:
     class TarziaContactLaw(PotentialOfContactLaw):
@@ -18,15 +20,27 @@ def make_slope_contact_law(slope: float) -> Type[ContactLaw]:
         def potential_normal_direction(
             var_nu: float, static_displacement_nu: float, dt: float
         ) -> float:
-            b = 5
+            b = B_COEF
             r = var_nu
             # EXAMPLE 11
-            # if r < b:
-            #     result = (r - b) ** 2
-            # else:
-            #     result = 1 - np.exp(-(r-b))
-            # EXAMPLE 13
-            result = 0.5 * (r - b) ** 2
+            if r < b:
+                result = 0.5 * (r - b) ** 2
+            else:
+                result = np.log((r - b) + 1)
+            result *= slope
+            return result
+
+        @staticmethod
+        def subderivative_normal_direction(
+            var_nu: float, static_displacement_nu: float, dt: float
+        ) -> float:
+            b = B_COEF
+            r = var_nu
+            # EXAMPLE 11
+            if r < b:
+                result = r - b
+            else:
+                result = 1 / (r - b  +1)
             result *= slope
             return result
 
@@ -39,22 +53,19 @@ class StaticPoissonSetup(PoissonProblem):
 
     @staticmethod
     def internal_temperature(x: np.ndarray, t: Optional[float] = None) -> np.ndarray:
-        if 0.4 <= x[0] <= 0.6 and 0.4 <= x[1] <= 0.6:
-            return np.array([-10.0])
-        return np.array([2 * np.pi**2 * np.sin(np.pi * x[0]) * np.sin(np.pi * x[1])])
+        return np.array([-4])
 
     @staticmethod
     def outer_temperature(x: np.ndarray, t: Optional[float] = None) -> np.ndarray:
-        if x[1] > 0.5:
-            return np.array([1.0])
-        return np.array([-1.0])
+        _y = x[1]
+        return np.array([_y * (_y - 1) * 32])
 
     boundaries: ... = BoundariesDescription(
         dirichlet=(
-            lambda x: x[0] == 0.0,
+            lambda x: x[1] == 0.0 or x[1] == 1.0,
             lambda x: np.full(x.shape[0], 5),
         ),
-        contact=lambda x: x[0] == 2.0,
+        # contact=lambda x: x[1] == 1.0,
     )
 
 
@@ -64,8 +75,8 @@ def main(config: Config):
 
     To see result of simulation you need to call from python `main(Config().init())`.
     """
-    alphas = [0.01, 0.1, 1, 10, 100, 1000, 10000]
-    ihs = [4, 8, 16, 32, 64, 128, 256]
+    alphas = [0.01, 0.1, 1, 10, 100, 1000, 10_000, 1_000_000, 1_000_000_000, np.inf]
+    ihs = [4, 8, 16, 32, 48, 72]
     alphas = alphas if not config.test else alphas[:1]
     ihs = ihs if not config.test else ihs[:1]
 
@@ -82,15 +93,16 @@ def main(config: Config):
 
 def simulate(config, alpha, ih):
     print(f"Simulate {alpha=}, {ih=}")
-    mesh_descr = CrossMeshDescription(
+    mesh_descr = RectangleMeshDescription(
         initial_position=None, max_element_perimeter=1 / ih, scale=[2, 1]
     )
     setup = StaticPoissonSetup(mesh_descr)
-    setup.contact_law = make_slope_contact_law(slope=alpha)
+    setup.contact_law_2 = make_slope_contact_law(slope=alpha)
 
-    runner = PoissonSolver(setup, "global")
+    solving_method = "schur" if alpha != np.inf else "direct"
+    runner = PoissonSolver(setup, solving_method)
 
-    state = runner.solve(verbose=True)
+    state = runner.solve(verbose=True, method="qsm")
 
     if config.outputs_path:
         with open(
@@ -114,9 +126,13 @@ def draw(config, alpha, ih):
     drawer = Drawer(state=state, config=config)
     drawer.cmap = "plasma"
     drawer.field_name = "temperature"
+    drawer.deformed_mesh_color = None
+    drawer.original_mesh_color = None
+
     drawer.draw(
+        title=f"alpha={alpha}, ih={ih}",
         show=config.show,
-        save=config.save,
+        save=not config.show,
         foundation=False,
         field_max=max_,
         field_min=min_,
